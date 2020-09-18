21dceb095bb446a9e79ab8539956d78208675793
Andrii Kovalov <jfkwww1@gmail.com>
2020-09-18 16:37:03 +0000

    hn_new_story sequential spider running

--- hacker_news/spiders/hacker_news_new_story.py
+++ hacker_news/spiders/hacker_news_new_story.py
@@ -1,12 +1,14 @@
 import json
 import os
+import sys
+import glob
 import scrapy
 import logging
 from scrapy import Request, signals
 from sqlalchemy import create_engine
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import sessionmaker, scoped_session, Session
-from datetime import datetime
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
 from sqlalchemy.exc import OperationalError
 
@@ -17,8 +19,29 @@ class HackerNewsNewStorySpider(scrapy.Spider):
     name = "hacker_news_new_story"
 
     def __init__(self, **kwargs):
-        #
         self.list_of_items = []
+        pid = str(os.getpid())
+        try:
+            self.process_name = glob.glob(
+                f"/usr/src/hacker_news_parser/{self.name}*"
+            )[0]
+            process_time = os.stat(self.process_name)[-1]
+            process_time = datetime.fromtimestamp(process_time)
+            process_time = datetime.strftime(
+                process_time, "%Y-%m-%d %H:%M:%S.%f"
+            )[:-3]
+            if datetime.now() - datetime.fromisoformat(process_time) >= timedelta(minutes=5):
+                logging.debug('file created more then 5 min ago')
+                os.unlink(self.process_name)
+                self.process_name = None
+            self.marker = True
+        except IndexError:
+            logging.debug('No process of this spider')
+            self.process_name = None
+        if not self.process_name:
+            with open(f'{self.name}-{pid}.txt', 'w+') as f:
+                f.close()
+                self.marker = False
     
 
     def db_connect(self):
@@ -101,20 +124,31 @@ class HackerNewsNewStorySpider(scrapy.Spider):
             logging.debug('No database found.')
         except AttributeError:
             logging.debug('No tables found.')
+        try:
+            self.process_name = glob.glob(
+                f"/usr/src/hacker_news_parser/{self.name}*"
+            )[0]
+            os.unlink(self.process_name)
+            logging.debug('Succesful spider run, closing spider.')
+        except IndexError:
+            logging.debug('No file found to delete.')
 
     def start_requests(self):
-        try:
-            db_info = self.db_connect()
-            self.HackerNewsNewStory = db_info['HackerNewsNewStory']
-            yield Request(
-                url="https://hacker-news.firebaseio.com/v0/newstories.json",
-                callback=self.parse,
-                dont_filter=True,
-            )
-        except OperationalError:
-            logging.debug('No database found.')
-        except AttributeError:
-            logging.debug('No tables found.')
+        if self.marker == True:
+            logging.debug('Runtime file found, closing spider.')
+        else:
+            try:
+                db_info = self.db_connect()
+                self.HackerNewsNewStory = db_info['HackerNewsNewStory']
+                yield Request(
+                    url="https://hacker-news.firebaseio.com/v0/newstories.json",
+                    callback=self.parse,
+                    dont_filter=True,
+                )
+            except OperationalError:
+                logging.debug('No database found.')
+            except AttributeError:
+                logging.debug('No tables found.')
 
     def parse(self, response):
         self.resp = json.loads(response.text)
